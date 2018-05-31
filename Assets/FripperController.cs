9e52dd0583b691b2de2cf58880e3c41a566bda19
lycoris25 <lycoris8460>
2018-05-31 21:37:11 +0900

    PinBall Lesson5課題提出
    スコア表示を追加しました。

---
+++ Assets/FripperController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+	private HingeJoint myHingeJoint;
+
+	private float defaulAngle = 20;
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		SetAngle(this.defaulAngle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag"){
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaulAngle);
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaulAngle);
+		}
+	}
+
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+				jointSpr.targetPosition = angle;
+				this.myHingeJoint.spring = jointSpr;
+	}
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+	public GameObject scoreText;
+
+	private int score = 0;
+
+	public void Score(int point){
+		this.score += point;
+		scoreText.GetComponent<Text>().text = score.ToString();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		this.scoreText = GameObject.Find ("ScoreText");
+
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+
+		Debug.Log (other.gameObject.tag);
+
+		if (other.gameObject.tag == "SmallStarTag"){
+			Score(10);
+		}else if(other.gameObject.tag == "LargeStarTag"){
+			Score(30);
+		}else if(other.gameObject.tag == "SmallCloudTag"){
+			Score(20);
+		}else if(other.gameObject.tag == "LargeCloudTag"){
+			Score(50);
+		}
+
+     }
+}
